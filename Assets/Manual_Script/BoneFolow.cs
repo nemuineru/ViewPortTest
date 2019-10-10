b6065a50cbd7dbca8ba2bca5b14057862d5f4bf8
nemuineru <34166165+nemuineru@users.noreply.github.com>
2019-10-10 19:19:50 +0900

    002

--- Assets/Manual_Script/BoneFolow.cs
+++ Assets/Manual_Script/BoneFolow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BoneFolow : MonoBehaviour {
 
     public Transform FollowTransform;
-    public Vector3 MoveGlobalposition, MoveGlovalrotation;
+    public Vector3 MoveGlobalposition, MoveGlobalrotation;
     [Range(0f, 1f)]
     public float FollowEasing, MoveEasing;
 
@@ -18,12 +18,12 @@ public class BoneFolow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        MoveGlovalrotation 
-            = new Vector3(Mathf.DeltaAngle(0f,MoveGlovalrotation.x), Mathf.DeltaAngle(0f, MoveGlovalrotation.y), Mathf.DeltaAngle(0f, MoveGlovalrotation.z));
+        MoveGlobalrotation 
+            = new Vector3(Mathf.DeltaAngle(0f,MoveGlobalrotation.x), Mathf.DeltaAngle(0f, MoveGlobalrotation.y), Mathf.DeltaAngle(0f, MoveGlobalrotation.z));
         if (FollowEasing <=0)
         {
             gameObject.transform.position = FollowTransform.position + MoveEasing * MoveGlobalposition;
-            gameObject.transform.eulerAngles = FollowTransform.eulerAngles + MoveEasing * MoveGlovalrotation;
+            gameObject.transform.eulerAngles = FollowTransform.eulerAngles + MoveEasing * MoveGlobalrotation;
 
             NonHandedPos = FollowTransform.position;
             NonhandedRot = FollowTransform.eulerAngles;
@@ -31,7 +31,7 @@ public class BoneFolow : MonoBehaviour {
         else
         {
             gameObject.transform.position = (1 - FollowEasing) * FollowTransform.position + FollowEasing * NonHandedPos + MoveEasing * MoveGlobalposition;
-            gameObject.transform.eulerAngles =  (1 - FollowEasing) * FollowTransform.eulerAngles + FollowEasing * NonhandedRot + MoveEasing * MoveGlovalrotation;
+            gameObject.transform.eulerAngles =  (1 - FollowEasing) * FollowTransform.eulerAngles + FollowEasing * NonhandedRot + MoveEasing * MoveGlobalrotation;
         }
     }
 }
